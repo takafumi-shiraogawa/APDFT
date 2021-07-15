8b6e9461acbeb7f5a40eaec37f8ea9d8202b0794
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2021-07-15 10:59:28 +0900

    update a code for generating APDFT inputs
    with different fraction numbers for finite derivatives
    of the perturbed electron density

--- src/gener_apdft_conf.py
+++ src/gener_apdft_conf.py
@@ -30,11 +30,31 @@ def gener_inputs(deltaZ, deltaR):
   return template.render(**env)
 
 
-def copy_commands(order1, order2):
+def copy_ingredients(order1, order2):
   # basedir = os.path.dirname(os.path.abspath(__file__))
   # copyfile = "%s/template/commands.sh" % basedir
-  copyfile = "template/commands.sh"
+
+  # Set a target directry
   copy_directry = "QM/delta-%s-%s" % (str(order1), str(order2))
+
+  # Copy commands.sh
+  copyfile = "template/commands.sh"
+  shutil.copy(copyfile, copy_directry)
+
+  # Copy imp_mod_cli1.sh
+  copyfile = "template/imp_mod_cli1.sh"
+  shutil.copy(copyfile, copy_directry)
+
+  # Copy imp_mod_cli2.sh
+  copyfile = "template/imp_mod_cli2.sh"
+  shutil.copy(copyfile, copy_directry)
+
+  # Copy n2.xyz
+  copyfile = "template/n2.xyz"
+  shutil.copy(copyfile, copy_directry)
+
+  # Copy n2_mod.xyz
+  copyfile = "template/n2_mod.xyz"
   shutil.copy(copyfile, copy_directry)
 
   return
@@ -44,6 +64,18 @@ def copy_commands(order1, order2):
 if os.path.isdir("QM/"):
   shutil.rmtree("QM/")
 
+# If all_commands.sh exists, it is removed.
+if os.path.isfile("all_commands.sh"):
+  os.remove("all_commands.sh")
+
+# If all_imp_mod_cli1.sh exists, it is removed.
+if os.path.isfile("all_imp_mod_cli1.sh"):
+  os.remove("all_imp_mod_cli1.sh")
+
+# If all_imp_mod_cli2.sh exists, it is removed.
+if os.path.isfile("all_imp_mod_cli2.sh"):
+  os.remove("all_imp_mod_cli2.sh")
+
 # Set parameters.
 change_frac_num = 5.0 * (10.0 ** (-8))
 eval_num = 7
@@ -52,6 +84,13 @@ eval_num = 7
 
 delta = 0.0
 count = 0
+
+# For all_commands.sh, all_imp_mod_cli1.sh, and all_imp_mod_cli2.sh
+all_commands = []
+all_imp1 = []
+all_imp2 = []
+
+print('')
 
 for i in range(eval_num):
   effect_change_frac_num = change_frac_num * (10.0 ** i)
@@ -70,7 +109,27 @@ for i in range(eval_num):
     # Make an input file
     inputfile = gener_inputs(delta, delta)
     with open("%s/apdft.conf" % path, "w") as inp:
-        inp.write(inputfile)
+      inp.write(inputfile)
 
     # Copy commands.sh
-    copy_commands(i + 1, j + 1)
+    copy_ingredients(i + 1, j + 1)
+
+    # For all_commands.sh, all_imp_mod_cli1.sh, and all_imp_mod_cli2.sh
+    all_commands.append("( cd %s && bash commands.sh )" % path)
+    all_imp1.append("( cd %s && bash imp_mod_cli1.sh )" % path)
+    all_imp2.append("( cd %s && bash imp_mod_cli2.sh )" % path)
+
+
+print('')
+
+# Write all_commands.sh
+with open("all_commands.sh", "w") as fh:
+  fh.write("\n".join(all_commands))
+
+# Write all_imp_mod_cli1.sh
+with open("all_imp_mod_cli1.sh", "w") as fh:
+  fh.write("\n".join(all_imp1))
+
+# Write all_imp_mod_cli2.sh
+with open("all_imp_mod_cli2.sh", "w") as fh:
+  fh.write("\n".join(all_imp2))
