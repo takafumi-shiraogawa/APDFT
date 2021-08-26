c9046dab72abdbe725a43a16ac1817b9c1eba041
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2021-08-26 15:11:32 +0900

    update a shell program for cleaning outputs

--- src/clean_results.sh
+++ src/clean_results.sh
@@ -5,6 +5,9 @@ rm energies.csv
 rm reference_contributions.csv
 rm target_contributions.csv
 rm total_contributions.csv
+rm atomic_forces.csv
+rm deriv_rho_contributions.csv
+rm hf_ionic_force_contributions.csv
 rm dipoles.csv
 rm commands.sh
 rm -rf QM/
