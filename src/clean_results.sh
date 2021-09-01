4cde43126396b14a0c6272e68cbeab872ef435e7
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2021-09-01 17:02:30 +0900

    update a shell program for cleaning outputs
    Newly remove forces.csv, ele_dipoles.csv, and nuc_dipoles.csv.

--- src/clean_results.sh
+++ src/clean_results.sh
@@ -9,5 +9,8 @@ rm atomic_forces.csv
 rm deriv_rho_contributions.csv
 rm hf_ionic_force_contributions.csv
 rm dipoles.csv
+rm nuc_dipoles.csv
+rm ele_dipoles.csv
+rm forces.csv
 rm commands.sh
 rm -rf QM/
