9385fc9479ce1fb172b1f5deddbb549b981d722c
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2021-12-29 02:53:20 +0900

    update an ASE interface for APDFT

--- src/apdft/ase/ase_apdft.py
+++ src/apdft/ase/ase_apdft.py
@@ -92,7 +92,7 @@ class mod_APDFT(FileIOCalculator):
   implemented_properties = ['energy', 'forces']
 
   # command = "( cd work/temp && bash imp_mod_cli1.sh && bash commands.sh && bash imp_mod_cli2.sh )"
-  command = "( cd work/temp && bash imp_mod_cli1.sh && div_QM.py 1 && bash imp_mod_cli2.sh)"
+  command = "( cd work/temp && bash imp_mod_cli1.sh && div_QM.py 8 && bash imp_mod_cli2.sh)"
   discard_results_on_any_change = True
 
   def __init__(self, *args, label='APDFT', num_opt_step = None, **kwargs):
@@ -118,9 +118,9 @@ class mod_APDFT(FileIOCalculator):
     handle_APDFT.copy_ingredients()
 
     # This is a bond length
-    coord = np.zeros(2)
+    coord = np.zeros(len(self.atoms.positions))
 
-    for i in range(2):
+    for i in range(len(self.atoms.positions)):
       coord[i] = self.atoms.positions[i, 2]
 
     handle_APDFT.gener_inputs(coord)
@@ -137,17 +137,17 @@ class mod_APDFT(FileIOCalculator):
     dict_total_energy = csv.DictReader(inp_total_energy)
     dict_atomic_force = csv.DictReader(inp_atomic_force)
 
-    atoms = np.zeros(2)
+    num_atoms = len(self.atoms.positions)
 
     pot_energy = 0
-    atom_forces = np.zeros((len(atoms), 3))
+    atom_forces = np.zeros((num_atoms, 3))
 
     apdft_order = 1
 
     # Obtain results
     pot_energy = handle_APDFT.get_target_value(
         "total_energy_order", dict_total_energy, apdft_order)
-    for i in range(len(atoms)):
+    for i in range(num_atoms):
       atom_forces[i, 2] = handle_APDFT.get_target_value(
           "ver_atomic_force_%s_order" % str(i), dict_atomic_force, apdft_order)
       inp_atomic_force.close()
@@ -163,7 +163,7 @@ class mod_APDFT(FileIOCalculator):
                     'forces': atom_forces * hb_to_ea,
                     'stress': np.zeros(6),
                     'dipole': np.zeros(3),
-                    'charges': np.zeros(len(atoms)),
+                    'charges': np.zeros(num_atoms),
                     'magmom': 0.0,
-                    'magmoms': np.zeros(len(atoms))}
+                    'magmoms': np.zeros(num_atoms)}
     handle_APDFT.save_results(self.num_opt_step)
