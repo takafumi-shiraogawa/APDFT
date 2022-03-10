0811cdcbd0be047aff4d6adb411e917116f5db9d
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2022-03-10 20:02:18 +0900

    fix a function of MPI parallelization

--- src/apdft/ase/ase_apdft.py
+++ src/apdft/ase/ase_apdft.py
@@ -99,7 +99,8 @@ class mod_APDFT(FileIOCalculator):
   # command = "( cd work/temp && bash imp_mod_cli1.sh && bash commands.sh && bash imp_mod_cli2.sh )"
   command = "( cd work/temp && bash imp_mod_cli1.sh && div_QM.py %s && bash imp_mod_cli2.sh )" % str(num_smp_core)
   # For MPI
-  # command = "( cd work/temp && bash imp_mod_cli1.sh && MPI4PY_MAX_WORKERS=32 mpiexec -np 1 python3 ~/my_code/APDFT/src/MPI_div_QM.py && bash imp_mod_cli2.sh )"
+  # num_mpi_proc = 50
+  # command = "( cd work/temp && bash imp_mod_cli1.sh && pre_MPI_div_QM.py %s && mpiexec -np %s python3 ~/my_code/APDFT/src/MPI_div_QM.py && bash imp_mod_cli2.sh )" % (str(num_mpi_proc), str(num_mpi_proc))
 
   discard_results_on_any_change = True
 
--- src/pre_MPI_div_QM.py
+++ src/pre_MPI_div_QM.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 
-from mpi4py import MPI
-import os
 import sys
-
-comm = MPI.COMM_WORLD
-mpi_rank = comm.Get_rank()
 
 def readlines_commands_file(path):
     with open('%s/commands.sh' % path, 'r') as file:
@@ -15,7 +10,7 @@ def save_commands_file(file_name, text):
     with open(file_name, 'w') as file:
       file.write(text)
 
-def fake_gener_commands_file(path, par_var):
+def gener_commands_file(path, par_var):
   par_var = int(par_var)
 
   commands_lines = readlines_commands_file(path)
@@ -25,28 +20,20 @@ def fake_gener_commands_file(path, par_var):
     tune_div_num = div_num
   else:
     tune_div_num = div_num + 1
-  # div_commands_lines = [commands_lines[i:i+tune_div_num]
-  #                       for i in range(0, len(commands_lines), tune_div_num)]
+  div_commands_lines = [commands_lines[i:i+tune_div_num]
+                        for i in range(0, len(commands_lines), tune_div_num)]
 
-  # for textidx, text in enumerate(div_commands_lines):
-  #   save_commands_file("%s/commands_%s.sh" % (path, str(textidx)), "".join(text))
+  for textidx, text in enumerate(div_commands_lines):
+    save_commands_file("%s/commands_%s.sh" % (path, str(textidx)), "".join(text))
 
   if len(commands_lines) % tune_div_num == 0:
     return len(commands_lines) // tune_div_num
   else:
     return (len(commands_lines) // tune_div_num) + 1
 
-def inp_commands_file(path, pos):
-  os.system("( cd %s && bash commands_%s.sh )" % (path, str(pos)))
 
 par_var = sys.argv[1]
 par_var = int(par_var)
 path = "."
 
-real_par_var = fake_gener_commands_file(path, par_var)
-real_par_var = int(real_par_var)
-
-if mpi_rank > real_par_var - 1:
-  pass
-else:
-  inp_commands_file(path, mpi_rank)
+real_par_var = gener_commands_file(path, par_var)
