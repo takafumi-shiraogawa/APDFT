1917abd69758cbd0054254e8e530d1567f5ea65e
Takafumi Shiraogawa <takafumishiraogawa@gmail.com>
2023-03-14 10:30:11 +0900

    fix a script for setting the environment

--- modify_install.sh
+++ modify_install.sh
@@ -1,4 +1,5 @@
-pip install basis_set_exchange Cython cclib colorama
-pip install structlog==19.1.0
-pip install pyscf==1.7.6
-pip install ase==3.22.1
+# Python 3.6.13 is used
+pip3 install jinja2 basis_set_exchange Cython numpy scipy h5py cclib coloramal pandas
+pip3 install structlog==19.1.0
+pip3 install pyscf==1.7.6
+pip3 install ase==3.22.1
